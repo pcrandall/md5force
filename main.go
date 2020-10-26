85bed65324838770d0b5a5f45dc18514d27b3da0
pcrandall <crandall.phillip@gmail.com>
2020-10-26 07:12:37 -0500

    cleanup

--- main.go
+++ main.go
@@ -10,66 +10,9 @@ import (
 	"time"
 )
 
-var CHARS = []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-var passwords []string
+var CHARS = []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+var HASHES []string
 var wg sync.WaitGroup
-
-func main() {
-	if loadHashes() {
-		return
-	}
-	fmt.Println("File with passwords loaded. We're gonna crack", len(passwords), "passwords!")
-	start := time.Now()
-	cont := 2
-	for len(passwords) > 1 {
-		fmt.Println("Searching for passwords at length: ", cont)
-		wg.Add(1)
-		go compute(0, cont, "", &wg)
-		wg.Wait()
-		cont++
-	}
-	elapsed := time.Since(start)
-	fmt.Println("Password's file cracked in:", elapsed)
-
-}
-
-func compute(prefix int, n int, a string, wgFather *sync.WaitGroup) {
-	defer wgFather.Done()
-
-	if prefix == n-1 {
-		for _, d := range CHARS {
-			password := fmt.Sprintf("%s%c", a, d)
-
-			if searchPassword(password) {
-				if len(passwords) == 0 {
-					return
-				}
-			}
-		}
-	} else {
-		for i := range CHARS {
-			wgFather.Add(1)
-			if prefix == 0 {
-				go compute(prefix+1, n, fmt.Sprintf("%s%c", a, CHARS[i]), wgFather)
-			} else {
-				compute(prefix+1, n, fmt.Sprintf("%s%c", a, CHARS[i]), wgFather)
-			}
-		}
-	}
-}
-
-func searchPassword(pass string) bool {
-	hash := fmt.Sprintf("%x", md5.Sum([]byte(pass)))
-	for i, value := range passwords {
-		if strings.Compare(hash, value) == 0 {
-			// Password found!
-			fmt.Println("Find Password:", pass, " with hash:", hash)
-			passwords = append(passwords[:i], passwords[i+1:]...)
-			return true
-		}
-	}
-	return false
-}
 
 func loadHashes() bool {
 	stream, err := ioutil.ReadFile("file.txt")
@@ -78,6 +21,60 @@ func loadHashes() bool {
 		return true
 	}
 	readstring := string(stream)
-	passwords = strings.Split(readstring, "\n")
+	HASHES = strings.Split(readstring, "\n")
 	return false
+}
+
+func buildPassword(prefix int, _passLen int, _passBuild string, wgFather *sync.WaitGroup) {
+	defer wgFather.Done()
+	if prefix == _passLen-1 {
+		for _, char := range CHARS {
+			password := fmt.Sprintf("%s%c", _passBuild, char)
+			if searchPassword(password) {
+				if len(HASHES) == 0 {
+					return
+				}
+			}
+		}
+	} else {
+		for i := range CHARS {
+			wgFather.Add(1)
+			if prefix == 0 {
+				go buildPassword(prefix+1, _passLen, fmt.Sprintf("%s%c", _passBuild, CHARS[i]), wgFather)
+			} else {
+				buildPassword(prefix+1, _passLen, fmt.Sprintf("%s%c", _passBuild, CHARS[i]), wgFather)
+			}
+		}
+	}
+}
+
+func searchPassword(pass string) bool {
+	hash := fmt.Sprintf("%x", md5.Sum([]byte(pass)))
+	for i, value := range HASHES {
+		if strings.Compare(hash, value) == 0 {
+			// Password found!
+			fmt.Println("Find Password:", pass, " with hash:", hash)
+			HASHES = append(HASHES[:i], HASHES[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	if loadHashes() {
+		return
+	}
+	fmt.Println("File with hashes loaded. We're gonna crack", len(HASHES)-1, "passwords!")
+	start := time.Now()
+	testLen := 2
+	for len(HASHES) > 1 {
+		fmt.Println("Searching for passwords at length: ", testLen)
+		wg.Add(1)
+		go buildPassword(0, testLen, "", &wg)
+		wg.Wait()
+		testLen++
+	}
+	elapsed := time.Since(start)
+	fmt.Println("Password's file cracked in:", elapsed)
 }
